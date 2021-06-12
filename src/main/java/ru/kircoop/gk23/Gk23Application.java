4f54c5136257adb376b44ae0d28964dfe9fb3de7
kirillfatality2@yandex.ru <171210Sss>
2021-06-12 11:47:54 +0300

    init new project

---
+++ src/main/java/ru/kircoop/gk23/Gk23Application.java
@@ -0,0 +1,13 @@
+package ru.kircoop.gk23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gk23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gk23Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kircoop/gk23/Gk23ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kircoop.gk23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gk23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
